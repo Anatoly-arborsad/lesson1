89c8087a9fa274c0ed8187b9cc210fe02f95b515
Anatoly-arborsad <osippoff@mail.ru>
2024-11-27 12:27:31 +0300

    Add files via upload

---
+++ Peremennie.py
@@ -0,0 +1,5 @@
+kolihestvo_vipolnennih_DZ=12
+kolihestvo_zatrahennih_hasov=1.5
+nazvanie_kursa="'Pyton',"
+vremja_na_odno_zadanie=(kolihestvo_zatrahennih_hasov/kolihestvo_vipolnennih_DZ)
+print('Kurs:', nazvanie_kursa,'vsego zadah:',kolihestvo_vipolnennih_DZ,',','zatraheno hasov:', kolihestvo_zatrahennih_hasov , ',', 'srednee vremja vipolnenie:', vremja_na_odno_zadanie)
